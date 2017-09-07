4352c238a936c8a76ff03b0bc0d6b751d88019e4
Ramkumar <ramkumar0316@gmail.com>
2017-09-07 17:05:38 +0530

    4th commit

--- web.js
+++ web.js
@@ -1,3 +1,14 @@
+/*blur and focus image*/
+$('#projects > li').hover(function(){
+  $(this).siblings().addClass('blur');
+}, function(){
+  $(this).removeClass('clicked').siblings().removeClass('blur');
+  
+});
+
+$('#projects > li').click(function(e){
+  $(this).addClass('clicked');
+});
 
 // Modal Image Gallery
 function onClick(element) {
